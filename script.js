f923cc5ae59f65cc9a85786fc81417b020fb6416
Saintlygaze <153154293+Saintlygaze@users.noreply.github.com>
2025-02-11 10:52:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 // complete this js code
-function Person(name, age) {}
+class person{
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+person.prototype.greet=function()
+{
+	console.log("hi my name is"+`${this.name}`+,i am `${this.age}`+years old);
+}
 
+class Employee extends person{
+	constructor(name,age,jobTitle){
+		super(name,age);
+		this.jobTitle = jobTitle;
+	}
+}
+
+
+person.prototype.jobGreet=function()
+{
+	console.log("hi my name is"+`${this.name}`+,"i am "`${this.age}`+"years old",
+			   "and my job title is"+`${this.jobTitle}`);
+}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
